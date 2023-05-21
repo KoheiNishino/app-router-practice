d18335f97557ceaddf0c1a2482af91edd64f81a1
KoheiNishino <koheinishino.95@gmail.com>
2023-05-21 17:32:02 +0900

    🧑‍💻 learn App Router

---
+++ src/app/common/components/index.tsx
@@ -0,0 +1,4 @@
+// Chakra UI のコンポーネントを使うたびに "use client" を宣言するのは手間がかかる
+// 本ファイルにて Chatra UI のコンポーネントを export して Client Component として使えるようにする
+"use client";
+export * from "@chakra-ui/react"
---
+++ src/pages/api/articles/[slug]/comments/index.ts
@@ -0,0 +1,24 @@
+import fs from "fs"
+import { NextApiRequest, NextApiResponse } from "next"
+
+const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const slug = req.query.slug;
+  if (req.method === "GET") {
+    await delay(3000);
+    const comments = fs.readFileSync("comments.json", "utf8");
+    const articles = fs.readFileSync("articles.json", "utf8");
+    const articleId = JSON.parse(articles).articles.find(
+      (a: any) => a.slug === slug
+    ).id;
+
+    const articleComments = JSON.parse(comments).comments.filter(
+      (c: any) => c.articleId === articleId
+    );
+    res.status(200).json(articleComments);
+  }
+}
